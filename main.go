43191938da8fe5a2bac7bed195bd4c64b258ea60
JSONhilder <jhilder95@gmail.com>
2021-03-27 14:22:32 +0200

    Created init function to handle config & db.

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"github.com/JSONhilder/strongbox/cmd"
+	"github.com/JSONhilder/strongbox/internal/database"
+	"github.com/JSONhilder/strongbox/internal/utils"
 )
+
+func init() {
+	// Setup/Run anything needed before cli executes
+	config, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Println("No config file found.")
+	}
+
+	database.OpenDb(config)
+}
 
 func main() {
 	cmd.Execute()
