1834ee9346ffe6ef4cf8cdd75a122ab003cd108f
JSONhilder <jhilder95@gmail.com>
2021-05-23 10:01:42 +0200

    Added print logo function.

---
+++ internal/utils/logo.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+)
+
+func PrintLogo() {
+	b, err := ioutil.ReadFile("./logo.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	logo := string(b)
+	for j := 0; j < len(logo); j++ {
+		r, g, b := rgb(j)
+		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, logo[j])
+	}
+	fmt.Println()
+}
+
+func rgb(i int) (int, int, int) {
+	var f = 0.1
+	return int(math.Sin(f*float64(i)+0)*127 + 128),
+		int(math.Sin(f*float64(i)+2*math.Pi/3)*127 + 128),
+		int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
+}
